14239d084474bbf25e5c762d884310c1c876ce2d
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-01-02 13:15:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -95,9 +95,8 @@ install_x-ui() {
     stop_x-ui
 
     if [ $# == 0 ]; then
-        #last_version=$(curl -Ls "https://api.github.com/repos/amclubs/am-serv00-x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
         #wget -N --no-check-certificate -O x-ui-${release}-${arch}.tar.gz https://github.com/amclubs/am-serv00-x-ui/releases/latest/download/x-ui-${release}-${arch}.tar.gz
-        curl -L -O x-ui-freebsd-amd64.tar.gz -# --retry 2 --insecure https://github.com/yonggekkk/x-ui-yg/releases/download/1.0/x-ui-freebsd-amd64.tar.gz
+        wget -N --no-check-certificate -O x-ui-freebsd-amd64.tar.gz -# --retry 2 --insecure https://github.com/yonggekkk/x-ui-yg/releases/download/1.0/x-ui-freebsd-amd64.tar.gz
         if [[ $? -ne 0 ]]; then
             echo -e "${red}下载 x-ui 失败，请确保你的服务器能够下载 Github 的文件${plain}"
             exit 1
